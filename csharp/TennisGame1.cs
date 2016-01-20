1ecb8c36b4cf45f24513c92c925418bcc92b555a
Matt Juskelis <matthewjuskelis@gmail.com>
2016-01-19 17:19:28 -0800

    Refactoring

--- csharp/TennisGame1.cs
+++ csharp/TennisGame1.cs
@@ -3,84 +3,95 @@ using NUnit.Framework;
 
 namespace Tennis
 {
-  class TennisGame1 : TennisGame
-  {
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private string player1Name;
-    private string player2Name;
-
-    public TennisGame1 (string player1Name, string player2Name)
+    class TennisGame1 : TennisGame
     {
-      this.player1Name = player1Name;
-      this.player2Name = player2Name;
-    }
+        private int player1Score = 0;
+        private int player2Score = 0;
+        private string player1Name;
+        private string player2Name;
 
-    public void WonPoint (string playerName)
-    {
-      if (playerName == "player1")
-        m_score1 += 1;
-      else
-        m_score2 += 1;
-    }
-
-    public string GetScore ()
-    {
-      String score = "";
-      int tempScore=0;
-      if (m_score1==m_score2)
-      {
-        switch (m_score1)
+        public TennisGame1(string player1Name, string player2Name)
         {
-        case 0:
-          score = "Love-All";
-          break;
-        case 1:
-          score = "Fifteen-All";
-          break;
-        case 2:
-          score = "Thirty-All";
-          break;
-        default:
-          score = "Deuce";
-          break;
-          
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
         }
-      }
-      else if (m_score1>=4 || m_score2>=4)
-      {
-        int minusResult = m_score1-m_score2;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
-      }
-      else
-      {
-        for (int i=1; i<3; i++)
+
+        public void WonPoint(string playerName)
         {
-          if (i==1) tempScore = m_score1;
-          else { score+="-"; tempScore = m_score2;}
-          switch(tempScore)
-          {
-          case 0:
-            score+="Love";
-            break;
-          case 1:
-            score+="Fifteen";
-            break;
-          case 2:
-            score+="Thirty";
-            break;
-          case 3:
-            score+="Forty";
-            break;
-          }
+            if (playerName == player1Name)
+                player1Score += 1;
+            else if (playerName == player2Name)
+                player2Score += 1;
         }
-      }
-      return score;
+
+        public string GetScore()
+        {
+            String score = "";
+            if (player1Score == player2Score)
+            {
+                switch (player1Score)
+                {
+                    case 0:
+                        return "Love-All";
+                    case 1:
+                        return "Fifteen-All";
+                    case 2:
+                        return "Thirty-All";
+                    default:
+                        return "Deuce";
+                }
+            }
+            else if (player1Score >= 4 || player2Score >= 4)
+            {
+                int scoreDifference = player1Score - player2Score;
+                if (scoreDifference == 1)
+                    return "Advantage player1";
+                else if (scoreDifference == -1)
+                    return "Advantage player2";
+                else if (scoreDifference >= 2)
+                    return "Win for player1";
+                else
+                    return "Win for player2";
+            }
+            else
+            {
+                switch (player1Score)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+
+                score += " - ";
+
+                switch (player2Score)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
+            return score;
+        }
     }
-  }
 
 }
 
